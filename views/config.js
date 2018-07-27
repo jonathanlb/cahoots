bf38f156d86a872db96c59c9b9547293890cd1f1
Jonathan Bredin <bredin@acm.org>
2018-07-27 15:44:45 -0500

    Configure fields fill width

--- views/config.js
+++ views/config.js
@@ -7,12 +7,18 @@ exports.render = function render() {
       <h2>Issue Configurator</h2>
       <div className="tally" >
         <h3>New Issue</h3>
-        <label for="issueNameField" >Issue:</label>
-        <input type="text" id="issueNameField" />
+        <label className="rightLabel" for="issueNameField" >
+          Issue:
+        </label>
+        <span className="rightSpanner" >
+          <input className="rightInput" type="text" id="issueNameField" />
+        </span>
         <br/>
-        <label for="participantNameField" >Participant:</label>
-        <input type="text" id="participantNameField"
-          value=${localStorage.displayName || ''}/>
+        <label className="rightLabel" for="participantNameField" >Participant:</label>
+        <span className="rightSpanner" >
+          <input className="rightInput" type="text" id="participantNameField"
+            value=${localStorage.displayName || ''}/>
+        </span>
         <br/>
         <input className="proposeButton"
           type="button"
@@ -27,20 +33,29 @@ exports.render = function render() {
 
       <div className="tally" >
         <h3>Existing Issues</h3>
-        <label for="datIssueUriField" >Dat:</label>
-        <input type="text" id="datIssueUriField"
-          value=${window.app.datUri} />
+        <label className="rightLabel" for="datIssueUriField" >
+          Dat:
+        </label>
+        <span className="rightSpanner" >
+          <input type="text" id="datIssueUriField"
+            className="rightInput"
+            value=${window.app.datUri} />
+        </span>
       </div>
 
       <div className="tally" >
         <h3>Join Issue</h3>
-        <label for="datUriField" >Writeable Dat:</label>
-        <input type="text" id="datUriField"
-          value=${window.app.datUri}
-          readonly />
+        <label className="rightLabel" for="datUriField" >Writeable Dat:</label>
+        <span className="rightSpanner" >
+          <input className="rightInput" type="text" id="datUriField"
+            value=${window.app.datUri}
+            readonly />
+        </span>
         <br/>
-        <label for="configUriField" >URI:</label>
-        <input type="text" id="configUriField" />
+        <label className="rightLabel" for="configUriField" >URI:</label>
+        <span className="rightSpanner" >
+          <input className="rightInput" type="text" id="configUriField" />
+        </span>
         <br/>
         <input className="proposeButton"
           type="button"
